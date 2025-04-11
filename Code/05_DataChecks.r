34ad18ea9419ffccd20e4a1af9711c90154b4f55
J <j.williams20@imperial.ac.uk>
2025-04-11 16:22:43 +0100

    Explore the potential for ABM speed up by limiting max step length (thus pixels considered per deer-step)

--- Code/05_DataChecks.r
+++ Code/05_DataChecks.r
@@ -60,7 +60,29 @@ df %>%
 
 #check project length
 interval(first(df$t1_), last(df$t2_)) /years(1)
-#check max step length
+
+
+
+# Considerations for max step length in ABM
 max(df$sl_)
+summary(df$sl_)
+
+# exclude x% longest steps?
+n=length(df$sl_)
+sort(df$sl_)[floor(n*0.9999):n]
+
+# look at stats of excluded step options
+summary(sort(df$sl_)[(floor(n*0.9999)-2):n])
+summary(sort(df$sl_)[(floor(n*0.999)-1):n])
+
+# explore computational implications
+getPixelsConsideredPerStep<-function(maxStep){ (maxStep^2*pi)/(100*100)}
+getPixelsConsideredPerStep(8650)
+getPixelsConsideredPerStep(4000)
+getPixelsConsideredPerStep(2600)
 
 
+# explore computational implications of sampling the max step length from the observed distribution
+# for each deer at each step
+randomisedMaxLengths<-sample(x=df$sl_, replace=TRUE, size=1000000)
+sum(getPixelsConsideredPerStep(randomisedMaxLengths))/length(randomisedMaxLengths)
